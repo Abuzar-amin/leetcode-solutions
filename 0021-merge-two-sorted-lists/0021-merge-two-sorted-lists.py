3fd622565b5d4c1e0034f3863c39d4a164930570
Abuzar-amin <133777283+Abuzar-amin@users.noreply.github.com>
2026-08-25 22:21:57 +0530

    Time: 5 ms (0.93%), Space: 19.2 MB (73.31%) - LeetHub

---
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        cur = dummy
+        while l1 or l2:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            if  l1 == None or l2 == None:
+                if l1:
+                    cur.next = ListNode(l1.val)
+                    cur = cur.next
+                    l1 = l1.next if l1 else None
+                else:
+                    cur.next = ListNode(l2.val)
+                    cur =  cur.next
+                    l2 = l2.next if l2 else None
+            elif v1 == v2:
+                cur.next = ListNode(v1)
+                cur = cur.next
+                cur.next = ListNode(v2)
+                cur = cur.next
+                l2 = l2.next if l2 else None
+                l1 = l1.next if l1 else None
+            elif v1 > v2:
+                cur.next = ListNode(v2)
+                cur = cur.next
+                l2 = l2.next if l2 else None
+            else:
+                cur.next = ListNode(v1)
+                cur = cur.next
+                l1 = l1.next if l1 else None
+            
+
+        return dummy.next
